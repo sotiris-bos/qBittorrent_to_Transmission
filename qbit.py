6e4196bd95b5f6a76bd9a019ba931b2359f7e966
sotiris-bos <39224671+sotiris-bos@users.noreply.github.com>
2023-07-08 03:17:59 +0300

    Update qbit.py
    Previous script was not working properly

--- qbit.py
+++ qbit.py
@@ -1,19 +1,23 @@
 import qbittorrentapi
-import shutil
 
+#This script is used with 3 torrent clients. One is the main qbittorrent client where everything is downloaded, then torrents are separated to public and private and their .torrent files are exported to a second and a third torrent client, one used for public torrents and one used for private. After the .torrent files have been created to the respective "watched" directory of each client, they are deleted from the first qbittorrent client. Thus, the first qbittorrent client has no torrents active, while the second and third can continue seeding the already downloaded torrents, with different speed and peer settings depending on if they are seeding public or private torrents. I use cron for running this python script once a day.
+
+# instantiate a Client using the appropriate WebUI configuration
 qbt_client = qbittorrentapi.Client(
-    host='YOUR QBITTORRENT IP HERE',
+    host='_YOUR QBITTORRENT IP OR HOSTNAME HERE_',
     port=8080,
-    username='USERNAME',
-    password='PASSWORD'
+    username='_YOUR QBITTORRENT USERNAME HERE_',
+    password='_YOUR QBITTORRENT PASSWORD HERE_'
  )
 
-# retrieve and show all torrents
+#Find completed/uploading public and private torrents and export their .torrent files to their respective location to be picked up by the "public" or "private" torrent client.
 for torrent in qbt_client.torrents_info():
         if torrent.state_enum.is_uploading or torrent.state_enum.is_complete: 
           if not torrent.properties.is_private:
-                shutil.copyfile(f'/var/lib/docker/volumes/qbittorrent/config/qBittorrent/BT_backup/{torrent.hash}.torrent', f'/mnt/PUBLIC_WATCHED_TRANSMISSION_DIR/{torrent.name}.torrent') # Change /var/lib/docker/volumes/qbittorrent/BT_backup to your according location if needed.
-                qbt_client.torrents_delete(torrent_hashes={torrent.hash})
+              with open(f'/mnt/media/torrents/watched/{torrent.name}.torrent', "wb") as file:  #change /mnt/media/torrents/watched/  to your second (public) torrent client's watched folder
+               file.write(qbt_client.torrents_export(torrent_hash={torrent.hash}))
+               qbt_client.torrents_delete(torrent_hashes={torrent.hash})
           elif torrent.properties.is_private:
-                shutil.copyfile(f'/var/lib/docker/volumes/qbittorrent/config/qBittorrent/BT_backup/{torrent.hash}.torrent', f'/mnt/PRIVATE_WATCHED_TRANSMISSION_DIR/{torrent.name}.torrent') # Change /var/lib/docker/volumes/qbittorrent/BT_backup to your according location if needed.
-                qbt_client.torrents_delete(torrent_hashes={torrent.hash})
+              with open(f'/mnt/media/torrents/private_watched/{torrent.name}.torrent', "wb") as file:  #change /mnt/edia/torrents/watched/  to your third (private) torrent client's watched folder
+               file.write(qbt_client.torrents_export(torrent_hash={torrent.hash}))
+               qbt_client.torrents_delete(torrent_hashes={torrent.hash})
